224398a6416635eef852937d3cc62672eccc020a
AtharvGaur <atharava112@gmail.com>
2026-07-24 23:10:41 +0530

    Add workspace and project routes to the Express server setup, enabling endpoints for workspace and project management. This enhances the application's routing structure for better organization and scalability.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes"
+import workspaceRoutes from "./routes/workspaceRoutes"
+import projectRoutes from "./routes/projectRoutes"
 
 const app = express();
 app.use(express.json());
 
 app.use("/auth", authRoutes)
+app.use("/workspaces", workspaceRoutes)
+app.use("/workspaces", projectRoutes)
 
 app.listen(3000, () => {
     console.log(`App running on PORT 3000`)
