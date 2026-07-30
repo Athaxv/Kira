03d3b80f849b8c8d0dce04fda47beae81d81698c
AtharvGaur <atharava112@gmail.com>
2026-07-30 13:38:43 +0530

    Add resend email functionality to job processor
    - Integrated the Resend API for sending emails in the email job processor.
    - Implemented email notifications for user registration and issue assignment.
    - Enhanced error handling for user and issue lookups in the database.

--- apps/backend/src/jobs/email/processor.ts
+++ apps/backend/src/jobs/email/processor.ts
@@ -1,8 +1,62 @@
 import type { Job } from "bullmq";
 import type { sendEmailJob } from "./types";
+import { NotFoundError } from "../../errors/notFound";
+import { prisma } from "@repo/db";
+import { resend } from "../../lib/resend";
 
 export async function processEmailJob(job: Job<sendEmailJob>){
-    console.log("Processing email");
+    if (job.name == "Welcome"){
+        const { userId } = job.data as { userId: string };
 
-    console.log("Job: ", job);
+        if (!userId) {
+            throw new NotFoundError("No user exists")
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+    
+        if (!user) {
+            throw new NotFoundError("No user exists")
+        }
+
+        await resend.emails.send({
+            from: "noreply@athaxv.me",
+            to: user.email,
+            subject: "Welcome, onboard",
+            text: "hey, thanks for registering with us"
+        })
+    }
+    else if (job.name === "Issue_assigned"){
+        const { assigneeId, issueId } = job.data as { assigneeId: string, issueId: string };
+
+        const assignee = await prisma.user.findUnique({
+            where: {
+                id: assigneeId
+            }
+        })
+
+        if (!assignee){
+            throw new NotFoundError("Assignee not found")
+        }
+
+        const issue = await prisma.issue.findUnique({
+            where: {
+                id: issueId
+            }
+        })
+
+        if (!issue){
+            throw new NotFoundError("No issue found")
+        }
+
+        await resend.emails.send({
+            from: "noreply@athaxv.me",
+            to: assignee.email,
+            subject: "Assigned New Issue",
+            text: `Hey, new issue is assigned to you, go check it out ${issue.title}`
+        })
+    }
 }
